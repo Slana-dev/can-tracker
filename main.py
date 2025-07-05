71199f276034274320d03c0f55b2076c88e61fda
Slana <ambrozslana@gmail.com>
2025-07-05 13:50:30 +0200

    angle calculator

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
+import math
 from ultralytics import YOLO
 import cv2
 
 sirina = 1280
 visina = 720
+verticalFov = 57
+horizontalFov = 88
+horizontalFov_rad = math.radians(horizontalFov)
+verticalFov_rad = math.radians(verticalFov)
 
-cap = cv2.VideoCapture(2)  
+
+focalX = (sirina / 2) / math.tan(horizontalFov_rad / 2)
+focalY = (visina / 2) / math.tan(verticalFov_rad / 2)
+
+
+cap = cv2.VideoCapture(1)  
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, sirina)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, visina)
-model=YOLO(r"best.pt")
+model=YOLO(r"best.pt") 
 cap.set(cv2.CAP_PROP_FPS, 60)
 
 
-
-#vrne vse razdalje
+# vrne vse razdalje
 def kalkulatorRazdalj(boxes, len):
     i = 0
     razdalje = []
@@ -24,16 +33,17 @@ def kalkulatorRazdalj(boxes, len):
         i+=1
     return razdalje
 
-#izracuna trenutno razdaljo
-def trenutnaRazdalja(minInde, boxes):
+
+# izracuna trenutno razdaljo
+def trenutnaRazdalja(minInde, boxes): 
     x = boxes[minInde][0]
     y = boxes[minInde][1]
     dolzina = abs(x- int(sirina) // 2) + abs(y - int(visina) // 2)
     return dolzina
 
-#najde indeks minimalne razdalje
+# najde indeks minimalne razdalje
 def minDolzina(dolzine, n):
-    min = 100000
+    min = 1000000
     i = 0
     minInde = -1
     for i in range(n):
@@ -43,11 +53,42 @@ def minDolzina(dolzine, n):
         i+=1
     return minInde
 
-#izvede ukaze
+# Izracuna kot
+def kotKalkulator(boxes, minInde, smer):
+    if smer == 'x':
+        x = boxes[minInde][0]
+        dolzina = abs(x- int(sirina) // 2)
+        kotStopinje = math.degrees(math.atan(dolzina / focalX))
+        return kotStopinje
+    
+    y = boxes[minInde][1]
+    dolzina = abs(y- int(visina) // 2)
+    kotStopinje = math.degrees(math.atan(dolzina / focalY))
+    return kotStopinje
+    
+# Smer premika
+def smerPremika(boxes, minInde, smer):
+    if smer == 'x':
+        x = boxes[minInde][0]
+        dolzina = (x- int(sirina) // 2)
+        if(dolzina > 0):
+            return 'd'
+        return 'a'
+        
+    
+    y = boxes[minInde][1]
+    dolzina = (y- int(visina) // 2)
+    if(dolzina > 0):
+        return 's'
+    return 'w'
+
+
+# izvede ukaze
 def izvedi(minInde, n):
     counter = 0
     delay = 20
     while True:
+        microsteping = '0'
         ret, frame = cap.read()
         if not ret:
             break
@@ -59,7 +100,7 @@ def izvedi(minInde, n):
             conf=0.1,
             iou=0.3,
             persist=True,
-            device='cuda',
+            #device='cuda',
             verbose= False
         )
 
@@ -75,22 +116,29 @@ def izvedi(minInde, n):
         if stBox != n:
             print("RESET\n") 
             return
-
+        
         boxes = results[0].boxes.xywh.cpu().numpy()
 
         # In case indexes shift
         if minInde >= len(boxes):
             print("RESET\n")  
             return
-
+        
+        
         # Move the robot
         if counter % delay == 0:
             razdalja = trenutnaRazdalja(minInde, boxes)
-            print("Razdalja:",razdalja)
-            # Ce razdalja manjsa vklopi microstepping
-            if razdalja < 200:
+            print("Razdalja:", razdalja)
+           
+            if razdalja < 250:
+                microsteping = '1'
                 print("microstepping")
-            print("sending info")
+
+            smerX = smerPremika(boxes, minInde, 'x')
+            smerY = smerPremika(boxes, minInde, 'y')
+            kotX = kotKalkulator(boxes, minInde, 'x')
+            kotY = kotKalkulator(boxes, minInde, 'y')
+            print("smer" , smerX, smerY,"kot: ", kotX, kotY)
 
         counter+=1
 
@@ -99,51 +147,57 @@ def izvedi(minInde, n):
             break
 
 
-#MAIN
-delay = 20
-frameCounter = 0
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        break
+def mainLoop():
+    delay = 30
+    frameCounter = 0
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
 
-    results = model.track(
-        source=frame,
-        tracker="bytetrack.yaml",
-        classes=[0],
-        conf=0.1,
-        iou=0.3,
-        persist=True,
-       #device='cuda',
-        verbose= False
-    )
-    
-    # Display
-    anotacija= results[0].plot()
-    cv2.circle(anotacija, (int(sirina) // 2, int(visina) // 2), 5, (0, 0, 255), -1)
-    cv2.imshow("preview", anotacija)
-   
-
-    if frameCounter % delay == 0:
-        n = len(results[0].boxes)
-        if(n != 0):
-            boxes = results[0].boxes.xywh.cpu().numpy()
-            # Vse razdalje in minIndeks
-            dolzine = kalkulatorRazdalj(boxes, n)
-            minDolzinaInde = minDolzina(dolzine, n)
-            # Izvedi ukaze za minIndeks
-            print("Najblizji:",minDolzinaInde, dolzine[minDolzinaInde])
-            izvedi(minDolzinaInde, n)
-        else:
-            print("FREE ROAM")
-
-
-    frameCounter+=1
-      
-    # Quit
-    if cv2.waitKey(1) == ord('q'):
-        break
+        results = model.track(
+            source=frame,
+            tracker="bytetrack.yaml",
+            classes=[0],
+            conf=0.1,
+            iou=0.3,
+            persist=True,
+            #device='cuda',
+            verbose= False
+        )
+        
+        # Display
+        anotacija= results[0].plot()
+        cv2.circle(anotacija, (int(sirina) // 2, int(visina) // 2), 5, (0, 0, 255), -1)
+        cv2.imshow("preview", anotacija)
     
 
-cap.release()
-cv2.destroyAllWindows()
+        if frameCounter % delay == 0:
+            n = len(results[0].boxes)
+            if(n != 0):
+                boxes = results[0].boxes.xywh.cpu().numpy()
+                # Vse razdalje in minIndeks
+                dolzine = kalkulatorRazdalj(boxes, n)
+                minDolzinaInde = minDolzina(dolzine, n)
+                # Izvedi ukaze za minIndeks
+                print("Najblizji:",minDolzinaInde, dolzine[minDolzinaInde])
+                izvedi(minDolzinaInde, n)
+            else:
+                print("FREE ROAM")
+
+
+        frameCounter+=1
+        
+        # Quit
+        if cv2.waitKey(1) == ord('q'):
+            break
+        
+
+    cap.release()
+    cv2.destroyAllWindows()
+    return
+
+
+
+
+mainLoop()
